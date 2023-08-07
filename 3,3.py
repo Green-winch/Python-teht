d60c41be537426663ab98b34085ddccf8bb11491
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:08:49 +0300

    Update 3,3.py

--- 3,3.py
+++ 3,3.py
@@ -1,3 +1,4 @@
+#3.3 Kirjoita ohjelma, joka laskee käyttäjän syöttämän arvon vähennyksen sadasta. Ohjelma tulostaa: ”Sadasta vähennettynä jää xxx”, korvaa xxx vähennyksellä.
 luku1 = input("Anna numero: ")
 luku1 = int(luku1)
 luku2 = 100
