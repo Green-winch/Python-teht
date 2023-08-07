60d340dde7300859b7b87b191405d6395a3149ac
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:14:11 +0300

    Update 4,5.py

--- 4,5.py
+++ 4,5.py
@@ -1,3 +1,4 @@
+#4.5 Kirjoita ohjelma, johon käyttäjä syöttää auton värin ja iän. Jos auto on nuorempi kuin 5 vuotta tai punainen, niin tulosta ”Ostetaan”, muulloin ”Ei osteta”.
 avari = input("Mikä on auton väri?: ")
 aian = input("Kuinka vanha auto on?: ")
 aian = int(aian)
