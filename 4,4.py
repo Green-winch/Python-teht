3417a19e5d5f31bf9eab525f60baa788ae3cb340
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:12:49 +0300

    Update 4,4.py

--- 4,4.py
+++ 4,4.py
@@ -1,3 +1,4 @@
+#4.4 Kirjoita ohjelma, johon käyttäjä syöttää iän. Määrittele itse ala- ja ylärajat. Jos ikä on rajojen sisällä, niin ohjelma tulostaa ”Rajojen sisällä”. Jos ikä on liian nuori, niin tulostaa ”Liian nuori” ja jos liian vanha, niin ”Liian vanha”.
 luku1 = input("Ikä: ")
 luku1 = int(luku1)
 if luku1 > 60:
