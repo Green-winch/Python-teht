866874da7facff3781920616ddd6b11f82d29e98
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:03:14 +0300

    Update 2,1.py

--- 2,1.py
+++ 2,1.py
@@ -1,4 +1,4 @@
-#Kirjoita ohjelma, jossa on muuttuja ja se tulostetaan. Sitten muuta muuttujan arvoa niin, että sen arvo kasvaa kaksin kertaiseksi. Tulosta muuttuja.
+#2.1 Kirjoita ohjelma, jossa on muuttuja ja se tulostetaan. Sitten muuta muuttujan arvoa niin, että sen arvo kasvaa kaksin kertaiseksi. Tulosta muuttuja.
 luku1 = 10
 print(luku1)
 print(luku1 * 2)
