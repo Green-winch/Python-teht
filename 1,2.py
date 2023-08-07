1617bc46f1bf7ba890c4f6e5fc23f39d7c2d289a
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 14:59:41 +0300

    Update 1,2.py

--- 1,2.py
+++ 1,2.py
@@ -1,3 +1,4 @@
+#1.2 Kirjoita ohjelma, jossa asetetaan kahteen muuttujaan arvoiksi haluamasi luvut. Tulosta niiden muuttujien summa.
 luku1 = input("Anna luku:")
 luku1 = int(luku1)
 luku2 = input("Anna toinen luku:")
