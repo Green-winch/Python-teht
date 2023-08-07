2cb8a0fb4de1d35c9b286a3041bf22b6df6e0e04
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:03:50 +0300

    Update 2,3.py

--- 2,3.py
+++ 2,3.py
@@ -1,3 +1,4 @@
+#2.3 Kirjoita ohjelma, jossa on kaksi muuttujaa, joiden arvoina on itse valitsemasi numerot. Laske ensimmäisen muuttujan arvoa kahdella ja sitten laske ensimmäisen muuttujan arvoa toisen muuttujan arvolla.
 luku1 = 10
 luku2 = 14
 print(luku1 * 2)
