aef12796b413c4c9294a95a38b64488c8a88f74e
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:11:11 +0300

    Update 4,2.py

--- 4,2.py
+++ 4,2.py
@@ -1,3 +1,4 @@
+# 4.2 Kirjoita ohjelma, joka kysyy käyttäjän ikää. Täysi-ikäiselle tulostetaan ”Tervetuloa” ja alaikäiselle ”Poistukaa”.
 luku1 = input("Mikä on sinun ikä?: ")
 luku1 = int(luku1)
 if luku1 >= 18:
