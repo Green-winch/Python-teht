2e1571947f4f1b93c20014001d6500b2c276026e
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:00:52 +0300

    Update 1,4.py

--- 1,4.py
+++ 1,4.py
@@ -1,3 +1,5 @@
+#1.4 Kirjoita ohjelma, jossa muuttujaan asetetaan luku arvo, tulosta muuttuja. Sitten aseta muuttujan arvoksi,
+jokin toinen luku ja tulosta se.
 luku1 = input("Anna luku:")
 luku1 = int(luku1)
 print(luku1)
