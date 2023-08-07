7a3805fbf8cf6a8beaeeaf8c6296bfdd30b5c46e
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-07 15:10:31 +0300

    Update 4,1.py

--- 4,1.py
+++ 4,1.py
@@ -1,3 +1,4 @@
+#4.1 Kirjoita seuraavanlainen ohjelma. Muuttuja faarao on arvoltaan 100. Kirjoita ehto, joka tulostaa ”Faarao on nuori”, jos faaraon arvo on alle tuhat.
 faarao = 100
 if faarao < 1000:
     print("Faarao on nuori.")
